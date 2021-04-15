4f29a146d95296425825291f2905d2aceca62680
ino-bar <50051423+ino-bar@users.noreply.github.com>
2021-04-15 12:35:24 +0100

    15-04-21 fixed syncfusion issue.

--- MarkingData.cs
+++ MarkingData.cs
@@ -17,6 +17,7 @@ namespace Marksheet_Maker_2._0
 
         public void CSVToMarkingData(string path)
         {
+            
             using (Stream inputStream = File.OpenRead(path))
             {
                 using (ExcelEngine excelEngine = new ExcelEngine())
@@ -32,6 +33,44 @@ namespace Marksheet_Maker_2._0
                                  .ToList();
                 }
             }
+            
+            /*
+            Microsoft.Office.Interop.Excel.Application objXL = null;
+            Microsoft.Office.Interop.Excel.Workbook objWB = null;
+            objXL = new Microsoft.Office.Interop.Excel.Application();
+            objWB = objXL.Workbooks.Open(path);
+            Microsoft.Office.Interop.Excel.Worksheet objSHT = objWB.Worksheets[1];
+
+            int rows = objSHT.UsedRange.Rows.Count;
+            int cols = objSHT.UsedRange.Columns.Count;
+            DataTable dt = new DataTable();
+            int noofrow = 1;
+
+            for (int c = 1; c <= cols; c++)
+            {
+                string colname = objSHT.Cells[1, c].Text;
+                dt.Columns.Add(colname);
+                noofrow = 2;
+            }
+
+            for (int r = noofrow; r <= rows; r++)
+            {
+                DataRow dr = dt.NewRow();
+                for (int c = 1; c <= cols; c++)
+                {
+                    dr[c - 1] = objSHT.Cells[r, c].Text;
+                }
+
+                dt.Rows.Add(dr);
+            }
+            objWB.Close();
+            objXL.Quit();
+            markingData = dt;
+            markingDataColumns = markingData.Columns.Cast<DataColumn>().ToList();
+            markingDataColumnHeadings = markingDataColumns.Cast<DataColumn>()
+                         .Select(x => x.ColumnName)
+                         .ToList();
+            */
         }
     }
 }
